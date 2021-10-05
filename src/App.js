9b6b3f03415cfaea3a4af7a3359965ca52506ecc
andzoni <lorenzoniandre@outlook.com>
2021-10-06 00:33:13 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Config from './pages/Config';
+import Play from './pages/Play';
 import './App.css';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/config" component={ Config } />
+      <Route path="/play" component={ Play } />
+      <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
+    </Switch>
   );
 }
-
-export default App;
